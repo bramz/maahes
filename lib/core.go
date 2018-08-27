8922f138263c6a77d8f4cb2adc0cd699d7bcaf66
bramz <brockramz@gmail.com>
2018-08-27 13:45:21 +0200

    Removed unstaged changes during compare/convert

--- lib/core.go
+++ lib/core.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package lib
 
 import (
@@ -54,60 +53,3 @@ func Parser(session *discordgo.Session, message *discordgo.MessageCreate) {
 
 	fmt.Printf("Message: %+v\n", message.Message)
 }
-=======
-package lib
-
-import (
-	"fmt"
-
-	"github.com/bramz/maahes/lib/commands"
-	"github.com/bwmarrin/discordgo"
-)
-
-func StartSession() {
-	discord, err := discordgo.New("Bot <yourtoken>")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	discord.AddHandler(Parser)
-
-	err = discord.Open()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer discord.Close()
-
-	<-make(chan struct{})
-}
-
-func Parser(session *discordgo.Session, message *discordgo.MessageCreate) {
-	maahes, err := session.User("@me")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	author := message.Author
-	if author.ID == maahes.ID || author.Bot {
-		return
-	}
-
-	content := message.Content
-
-	cmds := map[string]func() string{
-		"test": commands.TestCmd,
-		"theo": commands.TheoCmd,
-		//		"quit": commands.QuitCmd,
-	}
-
-	if string(content[0]) == "!" {
-		trigger := content[1:]
-		return
-	}
-
-	fmt.Printf("Message: %+v\n", message.Message)
-}
->>>>>>> refactor
