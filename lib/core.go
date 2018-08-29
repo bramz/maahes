d9ed1da71828662a9fdf7e5665443694103c07fa
bramz <brockramz@gmail.com>
2018-08-29 11:47:42 +0200

    Fix unstaged changes

--- lib/core.go
+++ lib/core.go
@@ -9,7 +9,7 @@ import (
 )
 
 func StartSession() {
-	discord, err := discordgo.New("Bot <yourtoken>")
+	discord, err := discordgo.New("Bot NDc2MzMzMTYyMzkwMzU1OTY4.DmVuFA.Qfc3-cu0lfzaym11Ee0V5_aOUQU")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -41,18 +41,19 @@ func Parser(session *discordgo.Session, message *discordgo.MessageCreate) {
 
 	content := message.Content
 
-	cmds := map[string]func() string{
-		"test": commands.TestCmd,
-		"theo": commands.TheoCmd,
+	cmds := map[string]func(string) string{
+		"theo":   commands.TheoCmd(content),
+		"define": commands.DefineCmd(content[1:]),
 		//		"quit": commands.QuitCmd,
 	}
 
 	if string(content[0]) == "!" {
 		ct := content[1:]
 		out := cmds[ct]
-		session.ChannelMessageSend(message.ChannelID, out())
+		session.ChannelMessageSend(message.ChannelID, out(content))
 	}
 
+	// colorize terminal output
 	green := color.New(color.FgGreen).SprintFunc()
 	red := color.New(color.FgRed).SprintFunc()
 	blue := color.New(color.FgBlue).SprintFunc()
