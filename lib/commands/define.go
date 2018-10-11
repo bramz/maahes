5b704b7d919009839ae657e8dd70941a1ef51cdd
bramz <brockramz@gmail.com>
2018-10-11 11:06:17 +0200

    Refactor, main, core, commands

--- lib/commands/define.go
+++ lib/commands/define.go
@@ -1,54 +1,19 @@
 package commands
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-)
 
-const (
-	URL = "https://api.urbandictionary.com/v0/define?term="
+	urbandict "github.com/davidscholberg/go-urbandict"
 )
 
 type DefineCmd struct {
 }
 
-type SearchResult struct {
-	Tags    []string
-	Results []Results `json:"list"`
-}
-
-type Results struct {
-	Author     string
-	Definition string
-	Example    string
-}
-
-func (d DefineCmd) Handle(search []string) string {
-	response, err := http.Get(URL + url.QueryEscape(search[1]))
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		fmt.Errorf("response was not valid: %d", response.StatusCode)
-	}
-
-	data, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	result := &SearchResult{}
-	err = json.Unmarshal(data, result)
+func (d DefineCmd) Handle(content []string) string {
+	def, err := urbandict.Define(string(content[1]))
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	first := result.Results
-	def := first[0].Definition
-
-	return def
+	return def.Definition
 }
--- maahes.go
+++ maahes.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/bramz/maahes/lib"
 )
 
 func main() {
-	lib.StartSession()
+	if len(os.Args) > 1 {
+		lib.StartSession(os.Args[1])
+	} else {
+		fmt.Print("you must provide a token\n")
+		os.Exit(0)
+	}
 }
